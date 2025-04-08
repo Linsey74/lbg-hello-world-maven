08b35d34a326e1ef1f67f487e897fa712ab84fb6
Linsey <Linsey@qa.com>
2025-04-08 11:57:17 +0100

    added names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Coco"));
+        System.out.println(sayHelloToSomeone("Mini"));
+        System.out.println(sayHelloToSomeone("Candy"));
+        System.out.println(sayHelloToSomeone("Tinker"));
         System.out.println(sayGoodbye());
     }
 
